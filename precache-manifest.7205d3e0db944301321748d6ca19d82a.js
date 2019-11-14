e1ff7ac283da81dc64447657860a9a40bd8bb738
Bilal <bilalm354@gmail.com>
2019-11-14 20:39:36 +0000

    Updates

---
+++ precache-manifest.7205d3e0db944301321748d6ca19d82a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c0aae466be9e79077ffbb06e529555",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "c5352194b54a4add1f3b",
+    "url": "/react-todo/static/css/main.9cf8aba1.chunk.css"
+  },
+  {
+    "revision": "6661a6b3803793804438",
+    "url": "/react-todo/static/js/2.d943883f.chunk.js"
+  },
+  {
+    "revision": "c5352194b54a4add1f3b",
+    "url": "/react-todo/static/js/main.86265596.chunk.js"
+  },
+  {
+    "revision": "460e1f2afce5d90d8bdc",
+    "url": "/react-todo/static/js/runtime-main.8c3598db.js"
+  }
+]);
